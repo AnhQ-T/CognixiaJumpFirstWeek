97c2e9614c3603b7c28c8e5fe9dd7ce1609698db
Disto <48456332+DistortedAxiom@users.noreply.github.com>
2021-05-27 12:24:33 -0400

    Fixed code

--- JumpMay21CoreJava/src/com/cognixia/jump/exercises/ArraysMatching.java
+++ JumpMay21CoreJava/src/com/cognixia/jump/exercises/ArraysMatching.java
@@ -5,17 +5,30 @@ public class ArraysMatching {
 	public static void main(String[] args) {
 		
 		// Initializing test cases
-		int[] arr1 = {3,2,5,1};
-		int[] arr2 = {5,2,1,3};
+		int[] arr1 = {3,2,1,1};
+		int[] arr2 = {1,2,1,3};
 		
 		int[] sortedArr1 = arraySort(arr1);
 		int[] sortedArr2 = arraySort(arr2);
+		
 		
 		if(arr1.length != arr2.length) {
 			System.out.println("The two arrays are not equal");
 		}
 		
-		else if (sortedArr1.equals(sortedArr2)) {
+		boolean isEqual = false;
+		
+		for (int y = 0; y < arr1.length; y++) {
+			if(sortedArr1[y] == sortedArr2[y]) {
+				isEqual = true;
+			}
+			
+			else {
+				isEqual = false;
+			}
+		}
+		
+		if (isEqual == true) {
 			System.out.println("The two arrays are equal");
 		}
 		
